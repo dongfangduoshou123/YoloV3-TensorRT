8f7b235411e31cf8501fa4a3f8412cf970bb0ea4
Learner0918 <fujiang_peng@163.com>
2020-01-06 12:52:02 +0800

    修改utils.h

--- utils.h
+++ utils.h
@@ -13,13 +13,5 @@
     }\
 }while(0)
 
-void initDevice(int devNum)
-{
-    int dev = devNum;
-    cudaDeviceProp deviceProp;
-    CHECK(cudaGetDeviceProperties(&deviceProp, dev));
-    printf("Using device %d : %s\n", dev, deviceProp.name);
-    CHECK(cudaSetDevice(dev));
-}
 
 #endif
